e812cdaf03ba0b9f6e022b54a9fe67ed6762d3bf
Alejandro <el_grande_ahc@hotmail.com>
2020-01-21 22:02:20 -0600

    30. @ViewChildren & QueryLists
    * Uso de @ViewChildren & QueryLists para 
    acceder a los hijos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, AfterContentInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChildren, QueryList, ChangeDetectorRef } from '@angular/core';
 import { SimpleAlertViewComponent } from './simple-alert-view/simple-alert-view.component';
 
 @Component({
@@ -6,25 +6,32 @@ import { SimpleAlertViewComponent } from './simple-alert-view/simple-alert-view.
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterContentInit {
+export class AppComponent implements AfterViewInit {
 
   public isAddTimerVisible: boolean = false;
-  public isEndTimerAlertVisible: boolean = false;
+  // public isEndTimerAlertVisible: boolean = false;
   public time:number = 0;
   public timers:Array<number> = [];
 
-  @ViewChild(SimpleAlertViewComponent) alert: SimpleAlertViewComponent;
+  // @ViewChild(SimpleAlertViewComponent) alert: SimpleAlertViewComponent;
+  @ViewChildren(SimpleAlertViewComponent) alerts: QueryList<SimpleAlertViewComponent>;
 
-  constructor() {
+  constructor(private cdRef:ChangeDetectorRef) {
     this.timers = [3, 20, 185];
+  }
 
-   }
-
-   ngAfterContentInit(): void {
-    console.log(this.alert);
-    this.alert.show();
-    this.alert.title = "aqui va el titulo";
-    this.alert.message = "aqui va el mensaje";
+   ngAfterViewInit(): void {
+    this.alerts.forEach( item => { 
+      if(!item.title){
+        item.title = "aqui va el titulo";
+        item.message = "aqui va el mensaje";
+      }
+    console.log(item);
+    // this.alert.show();
+    // this.alert.title = "aqui va el titulo";
+    // this.alert.message = "aqui va el mensaje";
+    });
+    this.cdRef.detectChanges();
   }
 
   showAddTimer(){
@@ -38,12 +45,14 @@ export class AppComponent implements AfterContentInit {
   }
 
   showEndTimerAlert(){
-    this.isEndTimerAlertVisible = true;
+    // this.isEndTimerAlertVisible = true;
+    // TODO: motrar de algun modo la alerta
+    this.alerts.first.show();
     console.log("showEndTimerAlert");
   }
   
   hideEndTimerAlert(){
-    this.isEndTimerAlertVisible = false;
+    // this.isEndTimerAlertVisible = false;
     console.log("hiddeEndTimerAlert");
   }
 
